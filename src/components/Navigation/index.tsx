8cc51ba384e7ce60f58c6b44dfb3570143fc107d
patrick-n4 <patrickndayambaje4@gmail.com>
2022-08-20 18:57:35 +0200

    I have added the navigational component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,39 @@
+import { Link } from "@mui/material";
 import React from "react";
 
+const links: {
+  href: string;
+  name: string;
+}[] = [
+  {
+    href: "/",
+    name: "Home"
+  },
+  {
+    href: "/chat",
+    name: "Chat"
+  },
+  {
+    href: "/auth/login",
+    name: "Login"
+  },
+  {
+    href: "/auth/signup",
+    name: "Signup"
+  }
+];
+
 const Navigation: React.FC = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="bg-blue-300 w-full h-16 flex items-center justify-center space-x-2 shadow-md">
+      {links.map((data: { href: string; name: string }, index) => {
+        return (
+          <Link href={data.href} key={index}>
+            {data.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 export default Navigation;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Navigation from "../../components/Navigation";
 const Home: React.FC = () => {
-  return <div className="w-full h-screen bg-red-300">Home</div>;
+  return (
+    <div className="w-full h-screen bg-red-300">
+      <Navigation />
+      <div>Home</div>
+    </div>
+  );
 };
 
 export default Home;
