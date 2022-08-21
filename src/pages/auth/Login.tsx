1aad5a814ac351b49b689ae65fc2faa6f237ab59
patrick-n4 <patrickndayambaje4@gmail.com>
2022-08-21 07:39:07 +0200

    I have updated the login styles

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,12 +5,15 @@ import { useNavigate } from "react-router-dom";
 const Login: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center">
+    <div className="flex w-full h-screen flex-col md:flex-row">
       <img
         src="https://images.unsplash.com/photo-1488998287214-1e668a8e0dc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        className="w-4/6 h-screen"
+        className="w-full h-1/2 md:h-screen md:w-3/5 lg:w-4/6 "
       />
-      <div className={`flex flex-col w-2/6 p-20`}>
+      <div
+        className={`flex flex-col w-full justify-center p-5 md:w-3/4 md:p-2 lg:w-2/6 lg:p-5 xl:p-7 2xl:p-10`}
+      >
+        <p className="text-center text-light-blue">Login to Chatsp</p>
         <form className="flex flex-col space-y-3">
           <TextField />
           <TextField />
@@ -24,7 +27,7 @@ const Login: React.FC = () => {
               className="text-[1.1em] hover:underline cursor-pointer"
               underline="none"
               onClick={() => {
-                navigate("/signup");
+                navigate("auth/signup");
               }}
             >
               Don't have account?
