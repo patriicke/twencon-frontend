37ababd7791a8bcbe4e1563c6478e20bb7daf9aa
patriicke <patrickndayambaje4@gmail.com>
2022-08-28 23:54:28 +0200

    Removed the unnecessary files from the file

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -120,7 +120,9 @@ const Footer = () => {
           <div className="mt-5 flex space-x-1 items-center">
             <h1 className="font-medium">Languge</h1>
             <Language />
-            <p className="opacity-60 border border-gray-500 p-1 px-2 rounded-md">en-US</p>
+            <p className="opacity-60 border border-gray-500 p-1 px-2 rounded-md">
+              en-US
+            </p>
           </div>
         </div>
       </div>
@@ -143,54 +145,3 @@ const Footer = () => {
   );
 };
 export default Footer;
-/* 
- <div className="h-72 md:flex md:flex-col md:justify-center md:h-52 md:items-center border overflow-hidden sm:absolute left-0 right-0 dark:bg-gray-900">
-      <div className="flex h-[78%] space-x-1 p-1 dark:text-white md:w-full   ">
-        <div className="w-1/2 h-full grid grid-cols-2 md:ml-10 lg:ml-52 ">
-          <div className="text-[0.7rem] md:text-[0.8em]">
-            <div className="font-bold">Overview</div>
-            {overview.map((data, index) => {
-              return <div key={index}>{data}</div>;
-            })}
-          </div>
-          <div className="text-[0.7rem] md:text-[0.8em]">
-            <div className="font-bold ">Community</div>
-            {community.map((data, index) => {
-              return <div key={index}>{data}</div>;
-            })}
-          </div>
-          <div className="text-[0.7rem] md:text-[0.8em]">
-            <div className="font-bold">Follow us</div>
-            <div className="flex md:space-x-2">
-              {userNavs.map((data, index) => {
-                return (
-                  <a href={data.link} target="_blank" key={index}>
-                    {data.icon}
-                  </a>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-        <div className="w-1/2 h-full ">
-          <div className="md:text-[1em]">
-            <div className="font-bold text-[0.8em]">Contact Us</div>
-            <div className="text-[0.7em]">Call: +250 78 626 7979</div>
-            <div className="text-[0.7em] flex items-center sm:space-x-1 flex-wrap">
-              <div className="0.7rem">Email:</div>
-              <Button
-                className="lowercase p-0 text-[0.7rem] md:text-[1em] text-light-blue"
-                href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKnKzdQbfqSPBFngwZvTbDjKqMwGqtWPvHBKckCrdlDxhXHqJSHVGkjMBFQxNzmlwTDrxG"
-                target={"_blank"}
-              >
-                patrickndayambaje4@gmail.com
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center text-[0.8rem] dark:text-white">
-        &copy;2021-{new Date().getFullYear()} All rights are reserved
-      </div>
-    </div>
-*/
