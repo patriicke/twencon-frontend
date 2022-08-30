ec7049ff2634c52a2349331e42401a104f141c8f
patriicke <patrickndayambaje4@gmail.com>
2022-08-30 20:12:55 +0200

    This is the home page for the application

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useUserData } from "../../hooks";
+import { userData as userDataAction } from "../../features/user/userSlice";
+export const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    useUserData(navigate, dispatch, userDataAction);
+  }, []);
+  return <div>Home</div>;
+};
+
+export default Home;
