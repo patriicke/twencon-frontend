e2ad9b36def7e1ee64b0c4082d960caac8da54c5
patriicke <patrickndayambaje4@gmail.com>
2022-09-01 00:49:41 +0200

    I have added the loader on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Pages from "./pages";
-import Loading from "./assets/loading/loading.gif";
-import Logo from "./assets/logo/twencon.svg";
-
 const App: React.FC = () => {
-  const [loaded, setStatus] = useState<boolean>(false);
-
-  document.onreadystatechange = () => {
-    setStatus(document.readyState === "complete");
-  };
-
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false);
+      document.querySelector(".loading-container")?.classList.add("hidden");
+    };
+  }, []);
   return (
     <>
-      <div
-        className={`w-full h-screen flex items-center justify-center static z-50 ${
-          loaded ? "" : "hidden"
-        }`}
-      >
-        <div>
-          <img src={Loading} alt="Loading..." className="w-10" />
-        </div>
-        <div className="absolute bottom-12">
-          <img src={Logo} alt="Loading..." className="w-36" />
-        </div>
-      </div>
-      <div className={`${loaded ? "hidden" : ""}`}>
+      <div className={`${loading ? "hidden" : ""}`}>
         <Pages />
       </div>
     </>
