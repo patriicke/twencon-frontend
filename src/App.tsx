61f0920296a095c04f0cba7711658e2f14d6a698
patriicke <patrickndayambaje4@gmail.com>
2022-09-01 13:21:44 +0200

    I have added the loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Pages from "./pages";
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    window.onload = () => {
-      setLoading(false);
-      document.querySelector(".loading-container")?.classList.add("hidden");
-    };
-  }, []);
   return (
     <>
-      <div className={`${loading ? "hidden" : ""}`}>
+      <div className={`${false ? "hidden" : ""}`}>
         <Pages />
       </div>
     </>
