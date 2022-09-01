1fd76bc28f7ed500d9431f3fb42994085db610ae
patriicke <patrickndayambaje4@gmail.com>
2022-09-01 22:20:01 +0200

    This is the Messages component

---
+++ src/components/Messages/Messages.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Messages: React.FC = () => {
+  return (
+    <div className="h-full bg-red-300 w-full">
+      <div>d</div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Messages;
