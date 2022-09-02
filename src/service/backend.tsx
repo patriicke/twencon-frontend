857030d7c6e957e82d464ac7473be8e1426f26f6
patriicke <patrickndayambaje4@gmail.com>
2022-09-02 15:25:39 +0200

    This is the backend addition

--- src/service/backend.tsx
+++ src/service/backend.tsx
@@ -1,4 +1,4 @@
-const backend: string = "http://localhost:5001";
+const backend: string = "https://twencon.herokuapp.com";
 export default backend;
 //https://twencon.herokuapp.com
 //http://localhost:5001
