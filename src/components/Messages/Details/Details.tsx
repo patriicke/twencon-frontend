ddc15380e63f4a8a3df39d339684c253b2d43a4b
patriicke <patrickndayambaje4@gmail.com>
2022-09-03 20:47:09 +0200

    These are the chat members

--- src/components/Messages/Details/Details.tsx
+++ src/components/Messages/Details/Details.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 
 const Details: React.FC = () => {
-  return <div className="w-1/4 h-full border">Details</div>;
+  const chatDetails: string[] = ["Chat Members", "Shared Files"];
+  const [current, setCurrent] = useState<number>(0);
+  return (
+    <div className="w-1/4 h-full ">
+      <div className="h-[8%] w-full flex">
+        {chatDetails.map((data, index) => {
+          return (
+            <div
+              className={`w-1/2 flex items-center justify-center ${
+                current == index ? "border-b-4" : ""
+              } border-light-blue`}
+              key={index}
+              onClick={() => setCurrent(index)}
+            >
+              <Button className="h-full w-full">{data}</Button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="h-[92%] w-full"></div>
+    </div>
+  );
 };
 
 export default Details;
