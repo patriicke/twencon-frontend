0080d15b4d558dc8f002b248c88c0507bc348e19
patriicke <patrickndayambaje4@gmail.com>
2022-09-03 21:06:07 +0200

    This is the shared files component

---
+++ src/components/Messages/Details/SharedFiles.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SharedFiles: React.FC = () => {
+  return (
+    <div className="w-full h-full py-4 opacity-50 text-center">
+      No shared files yet!
+    </div>
+  );
+};
+
+export default SharedFiles;
