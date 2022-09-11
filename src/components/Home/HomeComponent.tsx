3eaee5ef6ddbe70e5f2b16c4fb0e2cdbc31d12d1
patriicke <patrickndayambaje4@gmail.com>
2022-09-11 14:36:37 +0200

    This is the backend addition

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const HomeComponent = () => {
-  return <div>HomeComponent</div>;
+const HomeComponent: React.FC = () => {
+  return (
+    <div className="flex h-full">
+      <div className="w-2/3 border"></div>
+      <div className="w-1/3 border"></div>
+    </div>
+  );
 };
 
 export default HomeComponent;
