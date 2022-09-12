f978f23fedd409397b88fc987c17b099e14683ab
patriicke <patrickndayambaje4@gmail.com>
2022-09-12 09:33:45 +0200

    This is the suggestions component

---
+++ src/components/Home/SuggestionComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuggestionComponent: React.FC = () => {
+  return (
+    <div className="w-2/4 border flex items-center justify-center h-full">
+      <div className="h-full w-4/5"></div>
+    </div>
+  );
+};
+
+export default SuggestionComponent;
