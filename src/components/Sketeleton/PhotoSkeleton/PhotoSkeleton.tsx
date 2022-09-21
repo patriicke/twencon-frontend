1aeb813c45281deb10820c6fd32c34c4087e03fc
patriicke <patrickndayambaje4@gmail.com>
2022-09-21 22:52:13 +0200

    I have added the user account page

--- src/components/Sketeleton/PhotoSkeleton/PhotoSkeleton.tsx
+++ src/components/Sketeleton/PhotoSkeleton/PhotoSkeleton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const PhotoSkeleton: React.FC = () => {
   return (
     <div className="border-2 rounded-full border-gray-300">
-      <Skeleton variant="circular" className="w-10 h-10 " />
+      <Skeleton variant="circular" className="w-10 h-10" />
     </div>
   );
 };
--- src/pages/Account/UserAccount.tsx
+++ src/pages/Account/UserAccount.tsx
@@ -1,32 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-
+import { useGetPosts } from "../../hooks";
+import Person from "./../../assets/person/person.png";
 const UserAccount: React.FC = () => {
   const user = useSelector((state: any) => state?.user?.userData);
+  const [posts, setPosts] = useState<any>([]);
+  const getPosts = async () => {
+    const posts = await useGetPosts(setPosts);
+    console.log(posts);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <div className="lg:w-[80%] xl:w-[50%] m-auto h-full my-2">
       <div className="border h-[25em] w-full bg-gray-200 rounded-md flex flex-col">
         <div className="border rounded-full h-1/3 relative">
-          <img
-            src={user?.profile}
-            className="rounded-full absolute h-[10em] w-[10em] -bottom-10 left-2"
-          />
+          {user?.profile == "icon" ? (
+            <img
+              src={Person}
+              className="rounded-full absolute h-[10em] w-[10em] -bottom-10 left-2"
+            />
+          ) : (
+            <img
+              src={user?.profile}
+              className="rounded-full absolute h-[10em] w-[10em] -bottom-10 left-2"
+            />
+          )}
           <div className="rounded-full absolute right-3 bottom-3 border border-gray-500 p-2 px-4 cursor-pointer opacity-80 font-semibold">
             Edit your profile
           </div>
         </div>
         <div className="h-2/3 bg-white p-2 pt-12">
-          <div className="flex flex-col">
+          <div className="flex gap-2">
             <h1 className="font-semibold opacity-80 text-[1.3em]">
               {user?.fullname}
             </h1>
-            <h1 className="font-normal opacity-80 text-[1.3em] text-blue-500">
+            <h1 className="font-normal opacity-80 text-[1.3em] text-blue-600">
               @{user?.username}
             </h1>
           </div>
-          <div>
-            <div>0 followers</div>
-            <div>0 following</div>
+          <div className="flex py-2 gap-3 text-[1.2em]">
+            <div className="flex gap-2">
+              <span>0</span>
+              <span className="font-bold">followers</span>
+            </div>
+            <div className="flex gap-2">
+              <span>0</span>
+              <span className="font-bold">following</span>
+            </div>
+            <div className="flex gap-2">
+              <span>
+                {
+                  (posts as any).filter((post: any) => {
+                    return post?.owner?.email === user?.email;
+                  }).length
+                }
+              </span>
+              <span className="font-bold">posts</span>
+            </div>
           </div>
         </div>
       </div>
